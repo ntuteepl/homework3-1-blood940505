a8e5b2b1a63bdf299393c4eae1034eee7ae1b663
blood940505 <146065313+blood940505@users.noreply.github.com>
2023-10-23 21:45:21 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,30 @@
-
+#include <stdio.h>
+#include <stdbool.h>
+main()
+{
+int s,d1,d2;
+int a1,a2;
+scanf("%d %d",&a1,&a2);
+for(int i = 1; i < 5 ;i++){
+int q = a1 / i;
+d1 = d1 + q;
+}
+for(int i = 1; i < 5 ;i++){
+int q = a2 / i;
+d2 = d2 + q;
+}
+if(d1 > d2){
+    printf("%d",a2);
+}
+if(d1 < d2){
+    printf("%d",a1);
+}
+if(d1 == d2){
+    if(a1 > a2){
+        printf("%d",a2);
+    }
+    if(a1 < a2){
+        printf("%d",a1);
+    }
+}
+}
