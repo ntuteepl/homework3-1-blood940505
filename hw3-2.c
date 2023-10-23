c7b9b2bfa4350fd7e3167352dad655af07c650cb
blood940505 <146065313+blood940505@users.noreply.github.com>
2023-10-24 00:35:33 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -2,21 +2,49 @@
 #include <stdbool.h>
 main()
 {
-int s1,s2,s3,d1,d2,d3;
+int s1,s2,s3,d1,d2,d3,temp;
 scanf("%d %d %d %d %d %d",&s1,&d1,&s2,&d2,&s3,&d3);
+
+if(s1<s2)
+{
+    temp=s1;
+    s1=s2;
+    s2=temp;
+    temp=d1;
+    d1=d2;
+    d2=temp;
+}
+if(s1<s3)
+{
+    temp=s1;
+    s1=s3;
+    s3=temp;
+    temp=d1;
+    d1=d3;
+    d3=temp;
+}
+
+if(s2<s3)
+{
+    temp=s2;
+    s2=s3;
+    s3=temp;
+    temp=d2;
+    d2=d3;
+    d3=temp;
+}
+
 int car = 1;
-if(s2 <= d1){
+if(s2 > d1){
     car = car + 1;
 }
-if(s3 <= d2){
+if(s3 > d2){
     car = car + 1;
 }
-if(s3 >= d1){
+if(s3 >= d1 && s2>d1 &&  d3>d2){
     car = car - 1;
 }
-if(s2 >= d3){
-    car = car - 1;
-}
+
 printf("%d",car);
 
 
