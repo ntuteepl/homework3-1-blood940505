26f5ac9b8dd7a5505643bcbfcacfb3700c1bac3e
blood940505 <146065313+blood940505@users.noreply.github.com>
2023-10-23 21:44:58 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+main()
+{
+int s1,s2,s3,d1,d2,d3;
+scanf("%d %d %d %d %d %d",&s1,&d1,&s2,&d2,&s3,&d3);
+int car = 1;
+if(s2 <= d1){
+    car = car + 1;
+}
+if(s3 <= d2){
+    car = car + 1;
+}
+if(s3 >= d1){
+    car = car - 1;
+}
+printf("%d",car);
 
+
+}
