7c573e462853120c03f1013baf0110571fdcefcb
blood940505 <146065313+blood940505@users.noreply.github.com>
2023-10-23 21:44:38 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+#include <stdbool.h>
+main()
+{
+int number;
+scanf("%d",&number);
+bool ans = true;
+for (int i = 2;(i * i) <= number ;i++ ){
+    if ((number % i) == 0 ){
+        ans = false;
+    }
+}
+if(number == 1){
+    ans = true;
+}
+if(number == 2){
+    ans = true;
+}
+if(ans = true){
+printf("YES");
+}
+else if(ans = false){
+    printf("NO");
+}
+}
